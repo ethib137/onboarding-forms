3bff65de087383ee5cd84e0d3a0df8983f8f5138
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-04-19 17:16:02 +0400

    init mvc portlet

---
+++ modules/onboarding-form/onboarding-form-web/src/main/java/com/liferay/onboarding/form/portlet/constants/OnboardingFormPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.onboarding.form.portlet.constants;
+
+/**
+ * @author evanthibodeau
+ */
+public class OnboardingFormPortletKeys {
+
+	public static final String ONBOARDINGFORM =
+		"com_liferay_onboarding_form_portlet_OnboardingFormPortlet";
+
+}
